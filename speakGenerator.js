e5e8e13aa5bdd1a8ac0f784b8574a090d40780eb
unknown <Marthyn@live.nl>
2013-03-06 17:52:15 +0100

    Add TTS engine

---
+++ speakGenerator.js
@@ -0,0 +1,9 @@
+// eSpeak and other code here are under the GNU GPL.
+function generateSpeech(text, args) {
+  var self = { text: text, args: args, ret: null };
+  (function() {
+
+  }).call(self);
+  return self.ret;
+}
+
